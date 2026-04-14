d844fde631ad4ae118355dbfb400600041e5bb14
Agnieszka Jarosz <jaroszagnieszka88@gmail.com>
2026-04-14 11:09:22 +0200

    partials with vite-plugin-ejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { ViteEjsPlugin } from 'vite-plugin-ejs'
 
 export default defineConfig({
   root: 'src',
@@ -9,5 +10,10 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true
-  }
+  },
+  plugins: [
+    ViteEjsPlugin({
+      title: 'Best Shop'
+    })
+  ]
 })
